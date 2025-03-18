6ca71f37bc78af6c00200216b88d30ffef10c52a
KrishDhiman01 <hackyrotewiller@gmail.com>
2025-03-18 13:16:51 +0530

    Adding Numbers using Java

--- src/MarksCalculator.java
+++ src/MarksCalculator.java
@@ -2,29 +2,38 @@ import java.util.Scanner;
 
 public class MarksCalculator {
     public static void main(String[] args) {
-        System.out.println("This is A Student's Percentage Program");
-        System.out.println("Enter the marks of First Subject ");
+
+        System.out.print("Please Enter A Number :- ");
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        System.out.println("Enter the marks of Second Subject ");
-        Scanner sca = new Scanner(System.in);
-        float b = sca.nextFloat();
-        System.out.println("Enter the marks of Third Subject ");
-        Scanner scb = new Scanner(System.in);
-        float c = scb.nextFloat();
-        System.out.println("Enter the marks of Fourth Subject ");
-        Scanner scc = new Scanner(System.in);
-        float d = scc.nextFloat();
-        System.out.println("Enter the marks of Fifth Subject ");
-        Scanner scd = new Scanner(System.in);
-        float e = scd.nextFloat();
-        float sum = a + b + c + d + e;
-        float sum1 = sum/500;
-        float sum2 = sum1*100;
-        System.out.println("The Percentage of The Student is");
-        System.out.println(sum2);
+        System.out.println(sc.hasNextInt());
 
+        System.out.print("Please Enter A Number :- ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+        System.out.print("Please Enter A Number :- ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
 
-
+//        System.out.println("This is A Student's Percentage Program");
+//        System.out.println("Enter the marks of First Subject ");
+//        Scanner sc = new Scanner(System.in);
+//        float a = sc.nextFloat();
+//        System.out.println("Enter the marks of Second Subject ");
+//        Scanner sca = new Scanner(System.in);
+//        float b = sca.nextFloat();
+//        System.out.println("Enter the marks of Third Subject ");
+//        Scanner scb = new Scanner(System.in);
+//        float c = scb.nextFloat();
+//        System.out.println("Enter the marks of Fourth Subject ");
+//        Scanner scc = new Scanner(System.in);
+//        float d = scc.nextFloat();
+//        System.out.println("Enter the marks of Fifth Subject ");
+//        Scanner scd = new Scanner(System.in);
+//        float e = scd.nextFloat();
+//        float sum = a + b + c + d + e;
+//        float sum1 = sum/500;
+//        float sum2 = sum1*100;
+//        System.out.println("The Percentage of The Student is");
+//        System.out.println(sum2);
     }
 }
