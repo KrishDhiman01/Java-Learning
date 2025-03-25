cb91d55f2ec6c41846268845058dd6d5cd2d7322
KrishDhiman01 <hackyrotewiller@gmail.com>
2025-03-25 10:38:28 +0530

    Methods In JAVA

---
+++ src/Methods.java
@@ -0,0 +1,21 @@
+public class Methods {
+    public static void main(String[] args) {
+
+    }
+//    static void a1(){
+//        System.out.println("Hello World!");
+//    }
+//    static void a1(int a){
+//        System.out.println(a + " Hello World!");
+//    }
+//    static void arre(int arr[]){
+//        arr [0] = 98;
+//    }
+//
+//    public static void main(String[] args) {
+//        int arr [] = {20, 30, 40, 50, 60};
+//        arre(arr);
+//        System.out.println(arr[0]);
+//    }
+
+}
